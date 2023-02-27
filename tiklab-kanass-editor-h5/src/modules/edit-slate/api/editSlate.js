99ff94e17e9acd25e6f5e1fb4907fbafe51feec4
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-02-27 19:44:54 +0800

    修改文件名称，改成大驼峰

--- tiklab-kanass-editor-h5/src/modules/edit-slate/api/editSlate.js
+++
@@ -1,9 +0,0 @@
-import {serviceLoc} from "../../../common/untils/request";
-
-export function Upload(data){
-    return serviceLoc.request({
-        url: "/dfs/upload",
-        method: "post",
-        data 
-    })
-}
