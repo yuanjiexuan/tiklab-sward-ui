aa1f4b26df7544cd7dcd5dbcc7119d1b2c661f0a
袁婕轩 <yuanjiexuan@darthcloud.com>
2023-06-15 20:22:46 +0800

    发布版本

--- enviroment/enviroment_local.js
+++ enviroment/enviroment_local.js
@@ -6,7 +6,7 @@
  * @LastEditors: 袁婕轩
  * @LastEditTime: 2021-12-20 10:57:28
  */
-const api =  'http://192.168.10.4:8060/';
+const api =  'http://192.168.10.8:8060/';
 const base_url = JSON.stringify(api);
 
 
